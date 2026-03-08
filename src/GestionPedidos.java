6d0c743dc1aeafa5feae452f8b6ed0846a2dc41e
Nicolas Pacheco <nicolaspacsim@gmail.com>
2026-03-08 14:40:11 +0100

    10º: Añadido bucle principal en GestionPedidos para buscar clientes por teléfono

--- src/GestionPedidos.java
+++ src/GestionPedidos.java
@@ -32,15 +32,45 @@ public class GestionPedidos {
         Collections.sort(this.cartaProductos);
 
         // Creamos un cliente de prueba
-        Cliente c1 = new Cliente("Juan", "Perez", "776245688", "Calle Falsa 123");
+        Cliente c1 = new Cliente("Juan", "Perez", "776245688", "Calle 123");
 
         // Lo metemos en el HashMap. La clave es su teléfono
         this.clientes.put(c1.getTelefono(), c1);
     }
 
-    // --- MÉT ODO PRINCIPAL DEL PROGRAMA ---
+    // --- BUCLE PRINCIPAL ---
     public void iniciar() {
-        System.out.println("Bienvenido al sistema de pedidos.");
+        boolean salir = false;
+
+        // Bucle que se repite hasta que el usuario escriba 0
+        while (salir == false) {
+            System.out.print("\nINTRODUZCA TELÉFONO (0 SALIR) :");
+            String tlfInput = this.sc.nextLine();
+
+            // Limpiamos los espacios"
+            String tlfLimpio = tlfInput.replace(" ", "");
+
+            if (tlfLimpio.equals("0")) {
+                System.out.println("GRACIAS POR USAR NUESTRO SOFTWARE!.");
+                salir = true;
+            } else {
+                // Buscamos el cliente en nuestro HashMap usando el teléfono limpio
+                // El mét odo .get() devuelve el Cliente si lo encuentra, o null si no existe
+                Cliente clienteActual = this.clientes.get(tlfLimpio);
+
+                if (clienteActual == null) {
+                    System.out.println("ERROR: El cliente no existe!");
+                } else {
+                    this.crearPedido(clienteActual);
+                }
+            }
+        }
+    }
+
+    private void crearPedido(Cliente cliente) {
+        // De momento solo ponemos esto para probar que lo encuentra bien
+        System.out.println("Cliente encontrado: " + cliente.getNombre() + " " + cliente.getApellidos());
+        System.out.println("Próximamente: Lógica de añadir productos...");
     }
 
     // --- MAIN ---
